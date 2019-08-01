75debda566fc881959f548ad095cc94a0ce3da75
panagot <panagiotispollis@gmail.com>
2019-08-01 16:17:29 +0300

    Add files via upload

---
+++ homework-submission/homework1/a/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StaticList from "./StaticList";
+import DynamicList from "./DynamicList";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="todo-list">
+        <h1>Static List</h1>
+        <StaticList />
+        <h1>Dynamic List</h1>
+        <DynamicList />
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
---
+++ homework-submission/homework1/a/src/StaticList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StaticList(props) {
+    return(
+      <div className="todo-item">
+        Go to school.
+        Every day read and must finish my home work that they give me.
+
+      </div>
+    )
+}
+export default StaticList;
