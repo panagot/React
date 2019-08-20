40cf6b7c4d43158f65e6424fd3390f6d0c343070
panagot <panagiotispollis@gmail.com>
2019-08-20 17:12:34 +0300

    Update StaticList.js

--- homework-submission/homework2/src/StaticList.js
+++ homework-submission/homework2/src/StaticList.js
@@ -4,7 +4,7 @@ function StaticList(props) {
     return(
       <div className="todo-item">
         Go to school.
-        Every day read and must finish my home work that they give me.
+        I read every day cause I  must finish my home work that they give me.
 
       </div>
     )
