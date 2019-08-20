a40bfb15098d6e94576d46622e56ef9cf9f58eb1
panagot <panagiotispollis@gmail.com>
2019-08-20 17:10:39 +0300

    Update DynamicList.js

--- homework-submission/homework2/src/DynamicList.js
+++ homework-submission/homework2/src/DynamicList.js
@@ -1,50 +1,81 @@
 import React from 'react';
 
-const todoList = [
-    {
-        id: 1,
-        description: "Get out of bed",
-        deadline: "2017-09-13",
-        done: false
-    },
-    {
-        id: 2,
-        description: "Brush teeth",
-        deadline: "2017-09-14",
-        done: false
-    },
-    {
-        id: 3,
-        description: "Eat breakfast",
-        deadline: "2017-09-15",
-        done: false
-    }
-];
+//workingon must enable buttons to remove tasks.
+//also change parameters that will after return
 
-function TodoItem(props) {
-    return (
-        <div className="todo-item">
-        {props.status ? <p>ToDo: {props.item} / Deadline: {props.deadline}</p> : 
-        <p style={{textDecoration: "line-through"}}>ToDo: {props.item} / Deadline: {props.deadline}</p>}
-        </div>
-    )
-}
-
-class DynamicList extends React.Component {
+class ToDoDynamic extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: todoList
+            todos: [
+                {
+                    id: 1,
+                    description: "Get out of bed",
+                    deadline: "2017-09-13",
+                    done: false
+                },
+                {
+                    id: 2,
+                    description: "Brush teeth",
+                    deadline: "2017-09-14",
+                    done: false
+                },
+                {
+                    id: 3,
+                    description: "Eat breakfast",
+                    deadline: "2017-09-15",
+                    done: false
+                }
+              
+            ]
         }
+        this.addTodo = this.addTodo.bind(this)
+        this.removeTodo = this.removeTodo.bind(this)
     }
+
+
+    addTodo(event){
+        event.preventDefault();
+        const addTodoItem = {
+            id: this.state.todos,
+            description: event.target.description.value,
+            deadline: event.target.deadline.value,
+            done: false
+        }
+        this.setState({todos: this.state.todos.concat(addTodoItem)})
+    }
+
+    removeTodo(index){
+        const removeTodoItem = this.state.todos.filter(item => item.id !== index);
+        this.setState({todos: removeTodoItem})
+    }
+
+
     render() {
-        const toDos = this.state.todos.map(item => <TodoItem key={item.id} item={item.description} deadline={item.deadline} status={item.done} />);
+       
         return (
-            <div >
-                <p>{toDos}</p>
+            <div>
+                <div>
+                <form onSubmit = {this.addTodo}>
+                <input type="text" placeholder="Description" name="description"></input>
+                <input type="date" placeholder="Deadline" name="deadline"></input>
+                <input type="submit" value="Submit" ></input>
+                </form>
+                </div>
+                {this.state.todos.map((todoList) =>
+                <div className="todo-item" key={todoList.id}>
+                <p> {todoList.description} / {todoList.deadline} </p>
+                <button onClick={() => this.removeTodo(todoList.id)}>Remove</button>
+                </div>
+                )}
             </div>
         )
     }
+
 }
 
-export default DynamicList;
+
+
+
+
+    export default ToDoDynamic;
